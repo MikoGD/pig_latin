95791f7fe9e2d9540fb6271911fc47d9a185b57f
Mikael Escolin <mikaelescolin@gmail.com>
2020-06-12 02:14:13 +0100

    Finish basic functions
    Finished getting sentence. Used a struct to store the sentence. It
    stored the original sentence as a string, the words and the words
    converted in a vector.
    
    Function was added to get the individual words and remove the
    punctuation.
    
    Function to convert the words into pig-latin was added. A function
    called is_vowel() was created to help convert the words.
    
    Function was added to display the new converted words. However the
    punctuation was not preserved.

--- src/main.rs
+++ src/main.rs
@@ -8,126 +8,204 @@ TODO:
 - Functions:
   x Display_menu()
   x get_menu_option()
-  - get_sentence() -> Sentence
-  - get_words() -> Vec<String>
-  - convert_sentence(Vec<String>) -> Vec<String>
-  - display_sentence(Vec<String>)
+  x get_sentence() -> Sentence
+  x get_words(String) -> Vec<String>
+  ~ convert_sentence(Vec<String>) -> Vec<String>
+  x is_vowel(char) -> bool
+  x display_sentence(&Vec<String>)
+FIXME:
+- convert_sentence() not converting properly
+
 */
 use std::io::{self, prelude::*};
 
 enum MenuOption {
-  Enter,
-  Convert,
-  Display,
-  Exit,
+    Enter,
+    Convert,
+    Display,
+    Exit,
 }
 
 struct Sentence {
-  original: String,
-  words: Vec<String>,
-  converted_words: Vec<String>,
+    original: String,
+    words: Vec<String>,
+    converted_words: Vec<String>,
 }
 
 impl Sentence {
-  fn new() -> Sentence {
-    Sentence {
-      original: String::new(),
-      words: Vec::new(),
-      converted_words: Vec::new(),
+    fn new() -> Sentence {
+        Sentence {
+            original: String::new(),
+            words: Vec::new(),
+            converted_words: Vec::new(),
+        }
     }
-  }
 }
 
 fn main() {
-  loop {
-    // Display menu
-    display_menu();
+    let mut curr_sentence: Sentence = Sentence::new();
+    loop {
+        // Display menu
+        display_menu();
 
-    // Get menu option
-    let user_input = get_menu_option();
-    let curr_sentence: Sentence;
+        // Get menu option
+        let user_input = get_menu_option();
 
-    match user_input {
-      // Get sentence from user
-      MenuOption::Enter => curr_sentence = get_sentence(),
-      // Convert words to pig latin
-      MenuOption::Convert => continue,
-      // Display converted sentence
-      MenuOption::Display => continue,
-      // Display new sentence
-      MenuOption::Exit => {
-        println!("\nExiting...");
-        break;
-      }
+        match user_input {
+            // Get sentence from user
+            MenuOption::Enter => curr_sentence = get_sentence(),
+            // Convert words to pig latin
+            MenuOption::Convert => {
+                curr_sentence.converted_words = convert_words(&curr_sentence.words)
+            }
+            // Display converted sentence
+            MenuOption::Display => display_sentence(&curr_sentence),
+            // Display new sentence
+            MenuOption::Exit => {
+                println!("\nExiting...");
+                break;
+            }
+        }
     }
-  }
 }
 
 fn display_menu() {
-  println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-  println!("Welcome to pig latin converter");
-  println!("1) Enter sentence");
-  println!("2) Convert sentence");
-  println!("3) Display converted sentence");
-  println!("4) Exit");
-  print!("> ");
+    println!("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    println!("Welcome to pig latin converter");
+    println!("1) Enter sentence");
+    println!("2) Convert sentence");
+    println!("3) Display converted sentence");
+    println!("4) Exit");
+    print!("> ");
 
-  io::stdout().flush().ok().expect("Could not flush stdout");
+    io::stdout().flush().ok().expect("Could not flush stdout");
 }
 
 fn get_menu_option() -> MenuOption {
-  let user_input: MenuOption = loop {
-    let mut user_input = String::new();
+    let user_input: MenuOption = loop {
+        let mut user_input = String::new();
 
-    io::stdin()
-      .read_line(&mut user_input)
-      .expect("Failed to read input");
-    let user_input: MenuOption = match user_input.trim().parse() {
-      Ok(num) => match num {
-        1 => MenuOption::Enter,
-        2 => MenuOption::Convert,
-        3 => MenuOption::Display,
-        4 => MenuOption::Exit,
-        _ => {
-          println!("Error: invalid menu option");
-          println!("Please enter a valid menu option");
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read input");
+        let user_input: MenuOption = match user_input.trim().parse() {
+            Ok(num) => match num {
+                1 => MenuOption::Enter,
+                2 => MenuOption::Convert,
+                3 => MenuOption::Display,
+                4 => MenuOption::Exit,
+                _ => {
+                    println!("Error: invalid menu option");
+                    println!("Please enter a valid menu option");
 
-          continue;
-        }
-      },
-      Err(_) => {
-        println!("Error: invalid menu option");
-        println!("Please enter a valid menu option");
+                    continue;
+                }
+            },
+            Err(_) => {
+                println!("Error: invalid menu option");
+                println!("Please enter a valid menu option");
 
-        continue;
-      }
+                continue;
+            }
+        };
+
+        break user_input;
     };
 
-    break user_input;
-  };
-
-  user_input
+    user_input
 }
 
 fn get_sentence() -> Sentence {
-  let user_input = loop {
-    let mut user_input = String::new();
+    let user_input = loop {
+        let mut user_input = String::new();
 
-    io::stdin()
-      .read_line(&mut user_input)
-      .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
 
-    let user_input = user_input.trim().to_string();
+        let user_input = user_input.trim().to_string();
 
-    if user_input == "" {
-      println!("Input a sentence to convert");
-      continue;
+        if user_input == "" {
+            println!("Input a sentence to convert");
+            continue;
+        }
+
+        break user_input;
+    };
+
+    let mut sentence = Sentence::new();
+    sentence.original = user_input.to_string();
+    sentence.words = get_words(user_input.to_string());
+    sentence
+}
+
+fn get_words(sentence: String) -> Vec<String> {
+    let mut words = Vec::new();
+
+    for word in sentence.split(" ") {
+        let mut punctuation_index: usize = 0;
+        for (index, character) in word.chars().enumerate() {
+            let character = character as u32;
+            if (character > 96 && character < 123) || (character > 64 && character < 91) {
+                continue;
+            } else {
+                punctuation_index = index;
+            }
+        }
+
+        if punctuation_index > 0 {
+            words.push(word[0..punctuation_index].to_string());
+        } else {
+            words.push(word.to_string());
+        }
     }
 
-    break user_input;
-  };
+    words
+}
 
-  let mut curr_sentence = Sentence::new();
-  curr_sentence.original = user_input.to_string();
-  curr_sentence
+fn convert_words(words: &Vec<String>) -> Vec<String> {
+    let mut converted_words = Vec::new();
+    for word in words.iter() {
+        let mut converted_word = String::new();
+        let mut first_letter: char = 'z';
+        for (index, character) in word.chars().enumerate() {
+            if index == 0 {
+                if is_vowel(character) {
+                    converted_word.push(character);
+                }
+                first_letter = character;
+            } else if index == word.len() - 1 {
+                converted_word.push(character);
+
+                if is_vowel(first_letter) {
+                    converted_word.push_str(&format!("-hay"));
+                } else {
+                    converted_word.push_str(&format!("-{}ay", first_letter));
+                }
+            } else {
+                converted_word.push(character);
+            }
+        }
+
+        converted_words.push(converted_word);
+    }
+
+    converted_words
+}
+
+fn is_vowel(letter: char) -> bool {
+    match letter {
+        'a' => true,
+        'e' => true,
+        'i' => true,
+        'o' => true,
+        'u' => true,
+        _ => false,
+    }
+}
+
+fn display_sentence(sentence: &Sentence) {
+    for word in sentence.converted_words.iter() {
+        print!("{} ", word);
+    }
 }
